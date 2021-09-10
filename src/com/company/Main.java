545a9a24aa800e50bcc21fd2b7aea9bb312a00e8
quiteqiang <qiang_wang@taylor.edu>
2021-09-10 15:46:10 +0800

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//    Scanner scannner = new Scanner(System.in);
+//    byte age = scannner.nextByte();
+//    System.out.println("age  " + age);
+        int a = 2;
+        System.out.println();
+
+    }
+}
